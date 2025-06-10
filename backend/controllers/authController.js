01a002aa1e9afe7283b55e66c807b09907d53bcd
Samuel <Samkostofik@gmail.com>
2025-06-10 08:27:38 +0200

    jwt bcrpyt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,28 +1,60 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+
+const SECRET_KEY = process.env.JWT_SECRET;
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
+
   try {
     const existing = await User.findOne({ email });
-    if (existing) return res.status(400).json({ message: 'User already exists' });
+    if (existing) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
 
-    const user = await User.create({username, email, password }); 
-    res.status(201).json(user);
-  }catch (err) {
-  console.error(err);
-  res.status(500).json({ message: err.message || 'Server error' });
-}
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+   
+    const token = jwt.sign(
+      { userId: user._id, username: user.username },
+      SECRET_KEY,
+      { expiresIn: '1h' }
+    );
+
+    res.status(201).json({ message: 'User created', token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message || 'Server error' });
+  }
 };
-
 export const login = async (req, res) => {
   const { username, password } = req.body;
+
   try {
     const user = await User.findOne({ username });
-    if (!user || user.password !== password) {
+    if (!user) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
 
-    res.status(200).json({ message: 'Login successful', token: 'fake-jwt-token' });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.status(200).json({ message: 'Login successful', token });
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
   }
